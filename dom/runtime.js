f4bb3a48951a7fa444bddae4a41dcb4442e7c67a
Julien Quint <julien@igel.co.jp>
2013-03-15 19:27:56 +0900

    Runtime (HTML)

---
+++ dom/runtime.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var env = bender.init_environment();
+var args = flexo.get_args();
+if (args.href) {
+  var url = flexo.absolute_uri(window.document.baseURI, args.href);
+  env.load_component(url, function (component) {
+    env.render_component(component, document.body);
+  });
+} else {
+  document.body.appendChild(flexo.$("p.bender--runtime-message",
+    "Nothing to run. Please specify a Bender component to load with the ", 
+    flexo.$code("href"), " URL parameter."));
+}
+
