9cf7d1bef2d88d1883bfc7737386d56f1c9026ba
Julien Quint <julien@igel.co.jp>
2013-03-29 11:34:33 +0900

    More cleaning

--- t/a.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Array.prototype.forEach.call("abcde", function (x) {
-  bender.$[x] = Object.create(bender.instance);
-  bender.$[x].did_render = function () {
-    console.log("[{0}#{1}] did_render".fmt(x, this.seqno));
-  };
-});
--- ui/ui.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-if (typeof bender.ui !== "object") {
-  bender.ui = {};
-}
-
-bender.ui.button = Object.create(bender.instance);
