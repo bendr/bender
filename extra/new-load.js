9e7cdaaf40fc78980ae3f3b0ecfc05b439b86b36
Julien Quint <julien@igel.co.jp>
2013-06-20 19:41:42 +0900

    Seq for new load

--- extra/new-load.js
+++ extra/new-load.js
@@ -1,10 +1,10 @@
 (function (bender) {
 
-  var reduce = Array.prototype.reduce;
-
-  flexo.Promise = function (id) {
+  flexo.Promise = function () {
     this._then = [];
   };
+
+  var foreach = Array.prototype.forEach
 
   flexo.Promise.prototype.then = function (on_fulfilled, on_rejected) {
     var p = new flexo.Promise;
@@ -13,6 +13,16 @@
       this._resolved.bind(this).delay();
     }
     return p;
+  };
+
+  var reduce = Array.prototype.reduce;
+
+  flexo.Promise.prototype.seq = function (xs, f) {
+    return reduce.call(xs, function (p, x) {
+      return p.then(function (v) {
+        return f(x, v);
+      });
+    }, this);
   };
 
   flexo.Promise.prototype.fulfill = function (value) {
@@ -65,7 +75,10 @@
     this._then = [];
   };
 
-
+  // Make an asynchronous XMLHttpRequest for `uri`, with optional parameters
+  // `params` (known parameters: method, responseType, headers, data.) Return a
+  // promise which will be fulfilled with the result response, or rejected with
+  // an object containing a reason and the request that was made.
   flexo.ez_xhr = function (uri, params) {
     var req = new XMLHttpRequest;
     if (!params) {
@@ -85,10 +98,11 @@
       if (req.response != null) {
         promise.fulfill(req.response);
       } else {
-        promise.reject(req);
+        promise.reject({ reason: "missing response", request: req });
       }
     };
-    req.onerror = promise.reject.bind(promise, req);
+    req.onerror = promise.reject.bind(promise,
+        { reason: "XHR error", request: req });
     req.send(params.data || "");
     return promise;
   };
@@ -98,11 +112,25 @@
 
   bender.Environment = function (document) {
     this.document = document || window.document;
-    this.urls = [];
+    this.urls = {};
+  };
+
+  bender.Environment.prototype.load = function (url) {
+    return this.urls[url] || flexo.ez_xhr(url, { responseType: "document" })
+      .then(function (response) {
+        return this.deserialize(response.documentElement);
+      }.bind(this)).then(function (d) {
+        this.urls[url] = new flexo.Promise().fulfill(d);
+        d.url = url;
+        return d;
+      }.bind(this), function (reason) {
+        this.urls[url] = new flexo.Promise().reject(reason);
+        reason.environment = this;
+        throw reason;
+      }.bind(this));
   };
 
   bender.Environment.prototype.deserialize = function (node) {
-    console.log("Deserialize node {%0}%1".fmt(node.namespaceURI, node.localName));
     if (node instanceof window.Node) {
       if (node.nodeType == window.Node.ELEMENT_NODE) {
         if (node.namespaceURI == bender.ns) {
@@ -110,52 +138,20 @@
           if (typeof f == "function") {
             return f.call(this, node, parent);
           } else {
-            console.log("Unknown bender element %0 (skipped)"
+            // Unknown Bender element
+            console.warn("Unknow element in Bender namespace: %0"
                 .fmt(node.localName));
           }
         } else {
-          console.log("Foreign content (skipped)");
+          // Foreign content
         }
-      } else {
-        console.log("Text (skipped)");
+      } else if (node.nodeType == window.Node.TEXT_NODE ||
+          node.nodeType == window.Node.CDATA_SECTION_NODE) {
+        // Text node
       }
     } else {
-      throw "Deseralization error: expected an element; got: %0".fmt(node);
+      throw "Deseralization error: expected a node; got: %0".fmt(node);
     }
-  };
-
-  bender.Environment.prototype.deserialize.component = function (elem) {
-    console.log("Deserialize component", elem);
-    var component = new bender.Component(this);
-    // Deserialize the prototype of the component (from the href attribute),
-    // then all child nodes. At every step, a promise handles the content and
-    // return the component itself so that it can be safely returned regardless
-    // of the presence of a prototype or children. The reduce function iterates
-    // over all children and is initialized with the result of getting the
-    // prototype for the component.
-    // TODO attributes
-    // TODO check the prototype chain for loops
-    return reduce.call(elem.childNodes, function (p, ch) {
-      return p.then(function (component) {
-        var p = component.environment.deserialize(ch);
-        if (p instanceof flexo.Promise) {
-          return p.then(function (d) {
-            component.append_child(d);
-            return component;
-          });
-        } else {
-          component.append_child(p);
-          return component;
-        }
-      });
-    }, elem.hasAttribute("href") ?
-      flexo.ez_xhr(flexo.absolute_uri(elem.baseURI, elem.getAttribute("href")),
-        { responseType: "document" }).then(function (response) {
-          return component.environment.deserialize(response.documentElement);
-        }).then(function (prototype) {
-          component.$prototype = prototype;
-          return component;
-        }) : new flexo.Promise().fulfill(component));
   };
 
   // Load a component and return a promise.
@@ -167,10 +163,7 @@
       if (!env) {
         env = new bender.Environment;
       }
-      return flexo.ez_xhr(args.href, { responseType: "document" })
-        .then(function (response) {
-          return env.deserialize(response.documentElement);
-        });
+      return env.load(flexo.absolute_uri(env.document.baseURI, args.href));
     }
     return new flexo.Promise().reject("No href argument for component.");
   };
@@ -180,10 +173,40 @@
     this.children = [];
   };
 
+  bender.Environment.prototype.deserialize.component = function (elem) {
+    var component = new bender.Component(this);
+    // TODO attributes
+    // TODO check the prototype chain for loops
+    return (elem.hasAttribute("href") ?
+      this.load(flexo.absolute_uri(elem.baseURI, elem.getAttribute("href")))
+        .then(function (prototype) {
+          component.$prototype = prototype;
+          return component;
+        }) : new flexo.Promise().fulfill(component))
+    .seq(elem.childNodes, function (ch) {
+      var p = component.environment.deserialize(ch);
+      if (p instanceof flexo.Promise) {
+        return p.then(function (d) {
+          component.append_child(d);
+          return component;
+        });
+      } else {
+        component.append_child(p);
+        return component;
+      }
+    });
+  };
+
   bender.Component.prototype.append_child = function (child) {
-    console.log("+ add child", child);
     // TODO
+    this.children.push(child);
     return child;
+  };
+
+  bender.View = function () {};
+
+  bender.Environment.prototype.deserialize.view = function (elem) {
+    return new bender.View;
   };
 
 }(this.bender = {}));
