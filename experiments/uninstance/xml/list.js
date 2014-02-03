85448faefdd806b16a2b7d3e674273ad8f7bef05
Julien Quint <julien@igel.co.jp>
2014-02-03 14:55:46 +0900

    Modular list

--- experiments/uninstance/xml/list.js
+++ experiments/uninstance/xml/list.js
@@ -1,62 +1,29 @@
 "use strict";
 
-var fs = require("fs");
 var path = require("path");
 var flexo = require("flexo");
 
 exports.init = function (morbo) {
 
-  var list_xml = function (tr, pathname) {
-    try {
-      var dirname = tr.local_path(pathname);
-      morbo.promisify(fs.readdir, dirname).then(function (files) {
-        return flexo.collect_promises(files.map(function (filename) {
-          var filepath = path.normalize(path.join(dirname, filename));
-          return morbo.promisify(fs.lstat, filepath).then(function (stat) {
-            var a = {};
-            if (stat.isFile()) {
-              var m = filename.match(/\.xml$/);
-              if (m) {
-                return flexo.$li(flexo.$a({ href: filename }, filename), " ",
-                  flexo.$a({ href: "/bender.html?href=%0/%1"
-                    .fmt(pathname, filename), "class": "bender" }, "run"));
-              }
-            } else if (stat.isDirectory()) {
-              filename += "/";
-              a["class"] = "dir";
-            } else if (stat.isSymbolicLink()) {
-              a["class"] = "link";
-            }
-            a.href = filename;
-            return flexo.$li(flexo.$a(a, filename));
-          });
-        }));
-      }).then(function (files) {
-        files.unshift(flexo.$li(flexo.$a({ "class": "dir", href: "../" },
-              "parent directory")));
-        var relative = path.relative(tr.root, dirname);
-        tr.serve_html(morbo.html({ title: "Directory listing of %0/"
-          .fmt(relative) },
-          flexo.$style(
-            flexo.css("body", { "font-family": "Univers, Avenir, sans-serif" }),
-            flexo.css("a", { "text-decoration": "none", color: "#ff4040" }),
-            flexo.css("ul", { "list-style-type": "none", padding: 0 }),
-            flexo.css(".bender", { color: "#0b486b", "font-weight": "bold" }),
-            flexo.css(".dir", { "font-weight": "bold" }),
-            flexo.css(".link", { "font-style": "italic" })),
-          flexo.$ul(files.join(""))));
-      }).then(flexo.nop, function (err) {
-        console.log(err);
-        tr.serve_error(500);
-      });
-    } catch (e) {
-      console.log(e);
-      tr.serve_error(403);
+  var item = morbo.dir.item;
+  morbo.dir.item = function (transaction, dirname, filename, stat) {
+    if (stat.isFile()) {
+      var m = filename.match(/\.xml$/);
+      if (m) {
+        dirname = path.relative(transaction.root, dirname);
+        return flexo.$li(flexo.$a({ href: filename }, filename), " ",
+          flexo.$a({ href: "/bender.html?href=%0/%1"
+            .fmt(dirname, filename), "class": "bender" }, "⚑ run"));
+      }
     }
+    return item(transaction, dirname, filename, stat);
   };
 
-  exports.routes = [
-    ["^(/xml)/$", { GET: list_xml }]
-  ];
+  var style = morbo.dir.style;
+  morbo.dir.style = function (transaction) {
+    var css = style(transaction);
+    css.push(flexo.css(".bender", { color: "#a61416" }));
+    return css;
+  }
 
 };
