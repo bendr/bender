bf1d79fc7d514e5037457bdf90488c2cf7ebadbe
julien@igel.co.jp <julien@igel.co.jp>
2014-02-14 09:33:03 +0900

    Hello minimal

--- experiments/minimal/bender.js
+++ experiments/minimal/bender.js
@@ -1,16 +1,14 @@
 // Minimal implementation of the data/rendering model of Bender (without XML
 // serialization, syntactic sugar, &c.)
 
-/* global window */
+/* global bender, console, exports, flexo, global, require, window */
 // jshint -W097
 
 "use strict";
 
 if (typeof window === "object") {
-  // Looks like a browser, define the bender object acting as namespace
   window.bender = {};
 } else {
-  // Looks like node so require flexo and alias `exports`
   global.flexo = require("flexo");
   global.bender = exports;
 }
@@ -77,6 +75,12 @@ bender.Component = flexo._ext(bender.Node, {
     return bender.Node.init.call(this);
   },
 
+  clone: function () {
+    var clone = bender.Node.clone.call(this);
+    clone.view = this.view.clone();
+    return clone;
+  },
+
   add_child: function (child) {
     var scope = Object.getPrototypeOf(this.scope);
     Object.keys(Object.getPrototypeOf(child.scope)).forEach (function (id) {
@@ -94,31 +98,37 @@ bender.Component = flexo._ext(bender.Node, {
   },
 
   add_property: function (name) {
-    var property = Object.create(bender.Property).init(name, component);
+    var property = Object.create(bender.Property).init(name, this);
     this.property_definitions[name] = property;
     return property;
   },
 
   add_watch: function (watch) {
-    this.watches.push(watch);
+    this.watches.push(watch, this);
     watch.component = this;
     return watch;
   },
 
-  render: function (stack, index, target) {
-    stack = this.stack_views();
-    index = 0;
-    stack[index].render(stack, index, target);
+  render: function (target) {
+    var stack = this.stack_views();
+    stack[0].render(stack, 0, target);
   },
 
   stack_views: function () {
-    var prototype = Object.getPrototypeOf(this);
-    var stack = prototype.stack_views() : [];
+    var prototype = this.prototype;
+    var stack = prototype ? prototype.stack_views() : [];
     var view = this.view.clone();
     view.stack = stack;
-    stack.push(stack);
+    stack.push(view);
     return stack;
-  },
+  }
+});
+
+flexo.make_readonly(bender.Component, "prototype", function () {
+  var prototype = Object.getPrototypeOf(this);
+  if (prototype !== bender.Component) {
+    return prototype;
+  }
 });
 
 
@@ -200,19 +210,45 @@ bender.DOMElement = flexo._ext(bender.Element, {
     return bender.Element.init.call(this);
   },
 
+  attr: function (ns, name, value) {
+    if (arguments.length === 2) {
+      return this.attrs[ns] && this.attrs[ns][name];
+    }
+    if (!this.attrs[ns]) {
+      this.attrs[ns] = {};
+    }
+    this.attrs[ns][name] = value;
+    return this;
+  },
+
   render: function (stack, index, target) {
     this.rendered = target.appendChild(target.ownerDocument
       .createElementNS(this.ns, this.name));
-    // TODO
+    for (var ns in this.attrs) {
+      for (var name in this.attrs[ns]) {
+        this.rendered.setAttributeNS(ns, name, this.attrs[ns][name]);
+      }
+    }
+    this.children.forEach(function (child) {
+      child.render(stack, index, this.rendered);
+    }, this);
   }
 });
 
 
 bender.Attribute = flexo._ext(bender.Element, {
-  init: function (ns, name, attrs) {
+  init: function (ns, name) {
     this.ns = flexo.safe_string(ns);
     this.name = flexo.safe_string(name);
     return bender.Element.init.call(this);
+  },
+
+  render: function (stack, index, target) {
+    this.target = target;
+    this.target.setAttributeNS(this.ns, this.name,
+      this.children.reduce(function (text, child) {
+        return text + (typeof child.text === "function" ? child.text() : "");
+      }, ""));
   }
 });
 
@@ -230,7 +266,8 @@ flexo._accessor(bender.Text, "text", function (text) {
 
 
 bender.Watch = {
-  init: function () {
+  init: function (component) {
+    this.component = component;
     this.gets = [];
     this.sets = [];
     return this;
@@ -246,6 +283,13 @@ bender.Watch = {
     this.sets.push(set);
     set.watch = this;
     return set;
+  },
+
+  render: function () {
+    var w = Object.create(bender.WatchVertex).init(this);
+    this.gets.forEach(function (get) {
+      // TODO
+    });
   }
 };
 
@@ -290,5 +334,30 @@ bender.SetEvent = flexo._ext(bender.Set, {
   init: function (type) {
     this.type = flexo.safe_string(type);
     return bender.Set.init.call(this);
+  }
+});
+
+
+bender.Vertex = {
+  init: function () {
+    this.incoming = [];
+    this.outgoing = [];
+    return this;
+  },
+
+  add_outgoing: function (edge) {
+    this.outgoing.push(edge);
+    edge.source = this;
+    return edge;
+  }
+};
+
+
+bender.Vortex = flexo._ext(bender.Vertex);
+
+bender.WatchVertex = flexo._ext(bender.Vertex, {
+  init: function (watch) {
+    this.watch = watch;
+    return bender.Vertex.init.call(this);
   }
 });
